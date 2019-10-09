83d73fffff7d9ced02081a45ded062a152103618
Taran <thetaranneeld@gmail.com>
2019-10-09 13:59:08 -0600

    adding some helpers, still working on them

---
+++ schemes/scheme-model.js
@@ -0,0 +1,40 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('schemes');
+  }
+  
+  function findById(id) {
+    return db('schemes')
+      .where({ id })
+      .first();
+  }
+  
+  function findSteps(id) {
+    return
+  }
+  
+  function add(scheme) {
+    return db('schemes')
+      .insert(scheme, 'id')
+      .then(([id]) => {
+        return findById(id);
+      });
+  }
+
+  function update(changes, id) {
+      return
+  }
+  
+  function remove(id) {
+      return
+  }
